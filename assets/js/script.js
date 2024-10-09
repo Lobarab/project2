3aca06a4150337d9bf95e950fafc58f0fabb76ff
Lobar Abraeva <y526acx6@students.codeinstitute.net>
2024-10-09 16:58:37 +0000

    Add functions structure to script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function loadQuestion(){
+
+};
+
+function checkAnswer(){};
+
+function startTimer(){};
+
+function resetTimer(){};
+
+function timeUp(){};
+
+function showResults(){};
+
+function resetQuiz(){};
+
+function restartQuiz(){};
